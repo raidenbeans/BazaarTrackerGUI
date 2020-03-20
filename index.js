c00506e0d323865471fc38a2acf92ca2da477640
PrinceRaiden <raidencraftserver@gmail.com>
2020-03-20 10:46:11 -0700

    nice

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ loadJSONFile(__filesDir + __categoriesFileName, function (response) {
     createButtons('mining', categories.mining);
     createButtons('combat', categories.combat);
     createButtons('woodFishing', categories.woodFishing);
-//    createButtons('oddities', categories.oddities);
+    createButtons('oddities', categories.oddities);
     
     var itemCheckboxes = document.getElementsByClassName('itemCheckbox');
     for (chbx of itemCheckboxes) {
@@ -36,8 +36,12 @@ loadJSONFile(__filesDir + __categoriesFileName, function (response) {
                     }
                 }
                 
+                document.getElementById('datatypeDropdown').style.display = 'block';
+                document.getElementById('timeButtonsContainer').style.display = 'block';
                 updateCheckbox(true, this);
             } else {
+                document.getElementById('datatypeDropdown').style.display = 'none';
+                document.getElementById('timeButtonsContainer').style.display = 'none';
                 updateCheckbox(false, this);
             }
         });
@@ -123,21 +127,65 @@ function updateCategoryCheckbox(checked, chbx) {
 
 // canvas and graphing drawing
 const propertiesEnum = {
-    HIGHEST_BUY_PRICE: 'highestBuyPrice',
-    AVERAGE_BUY_PRICE: 'averageBuyPrice',
+    HIGHEST_BUY_PRICE: 'Highest Buy Price',
+    AVERAGE_BUY_PRICE: 'Average Buy Price',
     
-    LOWEST_SELL_PRICE: 'lowestSellPrice',
-    AVERAGE_SELL_PRICE: 'averageSellPrice',
+    LOWEST_SELL_PRICE: 'Lowest Sell Price',
+    AVERAGE_SELL_PRICE: 'Average Sell Price',
     
-    QUICK_BUY_PRICE: 'quickBuyPrice',
-    QUICK_BUY_VOLUME: 'quickBuyVolume',
-    QUICK_BUY_MOVING_WEEK: 'quickBuyMovingWeek',
-    QUICK_BUY_ORDERS: 'quickBuyOrders',
+    QUICK_BUY_PRICE: 'Quick Buy Price',
+    QUICK_BUY_VOLUME: 'Quick Buy Volume',
+    QUICK_BUY_MOVING_WEEK: 'Quick Buy Moving Week',
+    QUICK_BUY_ORDERS: 'Quick Buy Orders',
     
-    QUICK_SELL_PRICE: 'quickSellPrice',
-    QUICK_SELL_VOLUME: 'quickSellVolume',
-    QUICK_SELL_MOVING_WEEK: 'quickSellMovingWeek',
-    QUICK_SELL_ORDERS: 'quickSellOrders'
+    QUICK_SELL_PRICE: 'Quick Sell Price',
+    QUICK_SELL_VOLUME: 'Quick Sell Volume',
+    QUICK_SELL_MOVING_WEEK: 'Quick Sell Moving Week',
+    QUICK_SELL_ORDERS: 'Quick Sell Orders'
+}
+
+var propertiesToDisplay = new Array();
+var index = 0;
+for (key in propertiesEnum) {
+    var btn = document.createElement('label');
+    btn.setAttribute('id', key + 'Button');
+    document.getElementById('datatypeDropdown-content').appendChild(btn);
+    
+    var container = document.createElement('div');
+    container.setAttribute('border', '2px solid blue');
+    btn.appendChild(container);
+    
+    var p = document.createElement('p');
+    p.innerText = propertiesEnum[key];
+    p.textContent = propertiesEnum[key];
+    container.appendChild(p);
+    
+    var chbx = document.createElement('input');
+    chbx.setAttribute('type', 'checkbox');
+    if (index === 0) {
+        chbx.checked = true;
+        container.style.backgroundColor = '#39a9ef'
+        propertiesToDisplay.push(key);
+    }
+    chbx.addEventListener('change', function() {        
+        this.parentElement.style.backgroundColor = this.checked ? '#39a9ef' : 'white';
+        
+        var prop = this.parentElement.parentElement.id.replace('Button', '');
+        if (this.checked) {
+            propertiesToDisplay.push(prop);
+        } else {
+            for (var i = 0; i < propertiesToDisplay.length; i++) {
+                if (propertiesToDisplay[i] === prop) {
+                    propertiesToDisplay.splice(i, 1);
+                    break;
+                }
+            }
+        }
+        updateCanvas();
+    });
+    container.appendChild(chbx);
+    
+    index++;
 }
 
 function fixDPI() {
@@ -157,30 +205,48 @@ function fixDPI() {
 }
 
 window.addEventListener('resize', function(e) {
+    fixDPI();
     drawGraph();
 })
 
-var dataToDisplay = new Array();
-function updateCanvas(propertyType, selectedItemId) {
-    fixDPI();
+var selectedItemId
+var timePeriod = new Array()
+timePeriod.push({ days: 99999 });
+timePeriod.push({ days: -1 });
+var dataToDisplay = new Array()
+function updateCanvas() {
+    var propertyType;
+    if (propertiesToDisplay.length === 0) {        
+        for (var property in propertiesEnum) {
+            propertyType = property;
+        }
+    } else {
+        propertyType = propertiesToDisplay[0];
+    }
+    
     document.getElementById('loadingImg').style.display = 'block';
     loadJSONFile(__filesDir + selectedItemId.replace(':', '+') + '.product', function (responseText) {
         document.getElementById('loadingImg').style.display = 'none';
-        console.log(document.getElementById('loadingImg'));
         var json = JSON.parse(responseText);
         dataToDisplay = new Array();
         
+        var j = 0;
         for (var property in json.productArray) {
+            // if out of the time period, continue to next element
+            var timeElapsed = getTimeStampToTimeElapsed(property)
+            if (!(timeElapsed.days < timePeriod[0].days && timeElapsed.days > timePeriod[1].days)) continue;
+            
+            j++;
             var obj = json.productArray[property];
             
-            switch (propertyType) {
-                case propertiesEnum.HIGHEST_BUY_PRICE:                
+            switch (propertiesEnum[propertyType]) {
+                case propertiesEnum.HIGHEST_BUY_PRICE:
                     dataToDisplay.push({timeStamp: property, data: obj.buySummary[0].pricePerUnit});
                     break;
                 case propertiesEnum.AVERAGE_BUY_PRICE:
                     var averageBuyPrice = 0;
                     var i = 0;
-                    for (var buyData in obj.buySummary) {
+                    for (var buyData of obj.buySummary) {
                         averageBuyPrice += buyData.pricePerUnit;
                         i++;
                     }
@@ -194,7 +260,7 @@ function updateCanvas(propertyType, selectedItemId) {
                 case propertiesEnum.AVERAGE_SELL_PRICE:
                     var averageSellPrice = 0;
                     var i = 0;
-                    for (var sellData in obj.sellSummary) {
+                    for (var sellData of obj.sellSummary) {
                         averageSellPrice += sellData.pricePerUnit;
                         i++;
                     }
@@ -230,18 +296,18 @@ function updateCanvas(propertyType, selectedItemId) {
             }
         }
         dataToDisplay.sort(function(a, b) {
-            return a.data - b.data;
+            return a.timeStamp - b.timeStamp;
         });
         
-        drawGraph();        
+        fixDPI();
+        drawGraph();
     });
 }
 
 function drawGraph(extra) {
-    fixDPI();
     const graph = document.getElementById('graph');
     const graphWidth = graph.width;
-    const graphHeight = graph.height - 20; // - int so there is padding on the top
+    const graphHeightAdjusted = graph.height - 20; // - int so there is padding on the top
     const ctx = graph.getContext('2d');
     const xSpacing = graphWidth / dataToDisplay.length;
     
@@ -249,6 +315,12 @@ function drawGraph(extra) {
     ctx.moveTo(0, 0);
     ctx.lineTo(0, graph.height);
     ctx.lineTo(graphWidth, graph.height);
+    ctx.stroke();
+    
+    ctx.beginPath();
+    ctx.strokeStyle = 'black';
+    ctx.moveTo(0, graph.height / 2);
+    ctx.lineTo(graphWidth, graph.height / 2);
     ctx.stroke();
     
     // drawing the graph
@@ -266,7 +338,7 @@ function drawGraph(extra) {
         var timeStamp = dataToDisplay[i].timeStamp;
         var data = dataToDisplay[i].data;
         var x = xSpacing * i;
-        var y = graph.height - (data * graphHeight / highestY);
+        var y = graph.height - (data * graphHeightAdjusted / highestY);
         if (i === 0) {
             ctx.moveTo(0, y);
         } else {
@@ -275,6 +347,7 @@ function drawGraph(extra) {
     }
     
     ctx.stroke();
+    
     if (extra !== undefined) extra(graph, ctx);
 }
 
@@ -284,7 +357,8 @@ function updateSelectedButton(checked, btn) {
     if (checked) {
         txt = findItemName(btn.id.replace('Button', ''));
         document.getElementById('graphContainer').style.display = 'inline-block';
-        updateCanvas(propertiesEnum.HIGHEST_BUY_PRICE, btn.id.replace('Button', ''));
+        selectedItemId = btn.id.replace('Button', '');
+        updateCanvas();
     } else {
         txt = '';
         document.getElementById('graphContainer').style.display = 'none';
@@ -310,15 +384,18 @@ function findItemName(item) {
 }
 
 // canvas specific data on hover logic
-document.getElementById('graph').addEventListener('mousemove', function (event) {
+document.getElementById('graphContainer').addEventListener('mousemove', function(event) {
     const y = 20;
     
+    fixDPI();
     drawGraph(function(graph, ctx) {
         ctx.beginPath();
         
-        var x = event.clientX - graph.getBoundingClientRect().left;
+        var x = (event.clientX < graph.getBoundingClientRect.left) ? 0 : (event.clientX > graph.getBoundingClientRect.right) ? graph.getBoundingClientRect.right : event.clientX - graph.getBoundingClientRect().left;
+        var finalX = x;
         
-        var d, ts;
+        var d = 0;
+        var ts = 0;
         var lowestDist = 999999;
         for (var i = 0; i < dataToDisplay.length; i++) {
             var dist = Math.abs(graph.width / dataToDisplay.length * i - x)
@@ -327,27 +404,101 @@ document.getElementById('graph').addEventListener('mousemove', function (event) 
                 Math.abs(graph.width / dataToDisplay.length * i - x);
                 ts = dataToDisplay[i].timeStamp;
                 d = dataToDisplay[i].data;
+                
+                finalX = graph.width / dataToDisplay.length * i;
             }
         }
         
-        var textX = x;
+        var textX = finalX;
         if (x > graph.width / 2) {
             textX -= 200;
         }
         ctx.fillText('Data: ' + d, textX, y);
-        ctx.fillText('Timestamp: ' + getTimeStampToTimeElapsed(ts) + ' ago', textX, y * 2);
-        ctx.moveTo(x, 0);
-        ctx.lineTo(x, graph.height);
+        
+        var timeElapsed = getTimeStampToTimeElapsed(ts);
+        var str = '';
+        if (timeElapsed.days !== 0) {
+            str += timeElapsed.days + ' Days ';
+        }
+        if (timeElapsed.hours !== 0) {
+            str += timeElapsed.hours + ' Hours ';
+        }
+        str += timeElapsed.minutes + ' Minutes';
+        ctx.fillText('Timestamp: ' + str + ' Ago', textX, y * 2);
+        
+        ctx.moveTo(finalX, 0);
+        ctx.lineTo(finalX, graph.height);
         
         ctx.stroke();
     });
 })
 
 function getTimeStampToTimeElapsed(timeStamp) {
-    var curDate = new Date();
-    var elapsedTime = timeStamp - curDate.getMilliseconds();
+    var elapsedSeconds = (Date.now() - timeStamp) / 1000; // time in s
     
-    return Math.round(elapsedTime / 1000) + ' seconds';
+    var days = Math.floor(elapsedSeconds / 86400);
+    var hours = Math.floor((elapsedSeconds - (days * 86400 ))/3600)
+    var minutes = Math.floor((elapsedSeconds - (days * 86400 ) - (hours *3600 ))/60)
+    var secs = Math.floor((elapsedSeconds - (days * 86400 ) - (hours *3600 ) - (minutes*60)))
+    
+    var obj = {};
+/*    if (months !== 0) {
+        str += months + ' Months ';
+    }*/
+    obj.days = days;
+    obj.hours = hours;
+    obj.minutes = minutes;
+    
+    return obj;
 }
 
-document.getElementById('graph').addEventListener('mouseout', function() { drawGraph(); })
+document.getElementById('graph').addEventListener('mouseout', function() { fixDPI(); drawGraph(); })
+
+// time button logic
+var timeButtonCheckboxes = new Array()
+var timeButtonAll = document.getElementById('timeButtonAllChbx')
+var timeButtonSixM = document.getElementById('timeButtonSixMChbx')
+var timeButtonOneM = document.getElementById('timeButtonOneMChbx')
+var timeButtonOneW = document.getElementById('timeButtonOneWChbx')
+var timeButtonThreeD = document.getElementById('timeButtonThreeDChbx')
+var timeButtonOneD = document.getElementById('timeButtonOneDChbx')
+
+timeButtonCheckboxes.push(timeButtonAll)
+timeButtonCheckboxes.push(timeButtonSixM)
+timeButtonCheckboxes.push(timeButtonOneM)
+timeButtonCheckboxes.push(timeButtonOneW)
+timeButtonCheckboxes.push(timeButtonThreeD)
+timeButtonCheckboxes.push(timeButtonOneD)
+
+for (var timeButton of timeButtonCheckboxes) {
+    timeButton.addEventListener('change', function () {
+        if (this.checked) {    
+            for (var tb of timeButtonCheckboxes) {
+                if (tb !== this) {
+                    updateCheckbox(false, tb);
+                }
+            }
+            
+            updateCheckbox(true, this);
+            timePeriod = new Array();
+            if (this === timeButtonAll) {
+                timePeriod.push({ days: 99999 });
+            } else if (this === timeButtonSixM) {
+                timePeriod.push({ days: 180 });
+            } else if (this === timeButtonOneM) {
+                timePeriod.push({ days: 30 });
+            } else if (this === timeButtonOneW) {
+                timePeriod.push({ days: 7 });
+            } else if (this === timeButtonThreeD) {
+                timePeriod.push({ days: 3 });
+            } else if (this === timeButtonOneD) {
+                timePeriod.push({ days: 1 });
+            }
+            timePeriod.push({ days: -1 });
+            
+            if (selectedItemId !== undefined) updateCanvas();
+        } else {
+            this.checked = true;
+        }
+    })
+}
